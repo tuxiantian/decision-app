90f2d8c8ee115921d8db7e635218352f12d14036
tuxiantian <tuxiantian@163.com>
2024-10-22 22:32:42 +0800

    优化结构的显示

--- src/PairwiseComparison.js
+++ src/PairwiseComparison.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import ReactJson from 'react-json-view';
 import './App.css';  // 引入 CSS 文件
 import { API_BASE_URL } from './config';
 
