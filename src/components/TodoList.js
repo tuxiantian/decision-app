bf66b65c48f13152ae4af29a09a0969443a7a6b6
tuxiantian <tuxiantian@163.com>
2024-10-23 21:53:58 +0800

    显示已结束的

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { API_BASE_URL } from '../config'; 
+import '../App.css';  // 引入 CSS 文件
+import { API_BASE_URL } from '../config';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [completedTodos, setCompletedTodos] = useState([]);
+  const [endedTodos, setEndedTodos] = useState([]);
+  const [selectedTab, setSelectedTab] = useState('completed'); // 新增状态，用于切换选项卡
   const [completedPage, setCompletedPage] = useState(1);
+  const [endedPage, setEndedPage] = useState(1);
   const [totalCompletedPages, setTotalCompletedPages] = useState(1);
+  const [totalEndedPages, setTotalEndedPages] = useState(1);
   const pageSize = 10;
   const [newTodoName, setNewTodoName] = useState('');
   const [newTodoDueDate, setNewTodoDueDate] = useState('');
@@ -19,12 +24,17 @@ const TodoList = () => {
   useEffect(() => {
     fetchTodos();
     fetchCompletedTodos(completedPage);
+    fetchEndedTodos(endedPage);
   }, []);
 
   // 当 completedPage 发生变化时重新获取已完成的待办事项
   useEffect(() => {
     fetchCompletedTodos(completedPage);
   }, [completedPage]);
+
+  useEffect(() => {
+    fetchEndedTodos(endedPage);
+  }, [endedPage]);
 
   const fetchTodos = () => {
     axios.get(`${API_BASE_URL}/todos`)
@@ -53,6 +63,17 @@ const TodoList = () => {
 
   };
 
+  // 新增方法，获取已结束的 Todo
+  const fetchEndedTodos = () => {
+    axios.get(`${API_BASE_URL}/todos/ended`)
+      .then(response => {
+        setEndedTodos(response.data.todos);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the ended todos!', error);
+      });
+  };
+
   const handleNextCompletedPage = () => {
     if (completedPage < totalCompletedPages) {
       setCompletedPage(completedPage + 1);
@@ -62,6 +83,18 @@ const TodoList = () => {
   const handlePrevCompletedPage = () => {
     if (completedPage > 1) {
       setCompletedPage(completedPage - 1);
+    }
+  };
+
+  const handleNextEndedPage = () => {
+    if (endedPage < totalEndedPages) {
+      setEndedPage(endedPage + 1);
+    }
+  };
+
+  const handlePrevEndedPage = () => {
+    if (endedPage > 1) {
+      setEndedPage(endedPage - 1);
     }
   };
 
@@ -196,7 +229,7 @@ const TodoList = () => {
       </div>
       <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridTemplateRows: '1fr 1fr', gap: '20px', width: '80%', marginTop: '20px' }}>
         <div style={{ border: '1px solid #000', padding: '10px' }}>
-          <h3 style={{backgroundColor:'red'}}>重要且紧急</h3>
+          <h3 style={{ backgroundColor: 'red' }}>重要且紧急</h3>
           {todos.filter(todo => todo.importance && todo.urgency && todo.status === 'in_progress').map(todo => (
             <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
               <input
@@ -218,7 +251,7 @@ const TodoList = () => {
           ))}
         </div>
         <div style={{ border: '1px solid #000', padding: '10px' }}>
-          <h3 style={{backgroundColor:'orange'}}>重要但不紧急</h3>
+          <h3 style={{ backgroundColor: 'orange' }}>重要但不紧急</h3>
           {todos.filter(todo => todo.importance && !todo.urgency && todo.status === 'in_progress').map(todo => (
             <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
               <input
@@ -238,7 +271,7 @@ const TodoList = () => {
           ))}
         </div>
         <div style={{ border: '1px solid #000', padding: '10px' }}>
-          <h3 style={{backgroundColor:'#FFD700'}}>不重要但紧急</h3>
+          <h3 style={{ backgroundColor: '#FFD700' }}>不重要但紧急</h3>
           {todos.filter(todo => !todo.importance && todo.urgency && todo.status === 'in_progress').map(todo => (
             <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
               <input
@@ -258,7 +291,7 @@ const TodoList = () => {
           ))}
         </div>
         <div style={{ border: '1px solid #000', padding: '10px' }}>
-          <h3 style={{backgroundColor:'green'}}>不重要也不紧急</h3>
+          <h3 style={{ backgroundColor: 'green' }}>不重要也不紧急</h3>
           {todos.filter(todo => !todo.importance && !todo.urgency && todo.status === 'in_progress').map(todo => (
             <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
               <input
@@ -278,37 +311,92 @@ const TodoList = () => {
           ))}
         </div>
       </div>
-      <div style={{ marginTop: '20px', width: '80%' }}>
-        <h3>Completed Todos</h3>
-        {completedTodos.map(todo => (
-          <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-            <div style={{
-              marginBottom: '10px',
-              padding: '10px',
-              borderRadius: '5px',
-              backgroundColor:
-                todo.importance && todo.urgency ? 'red' :
-                  todo.importance && !todo.urgency ? 'orange' :
-                    !todo.importance && todo.urgency ? '#FFD700' : 'green',
-              color:
-                todo.importance && todo.urgency ? 'white' :
-                  todo.importance && !todo.urgency ? 'white' :
-                    !todo.importance && todo.urgency ? 'black' : 'white'   
-            }}>
-              <strong style={{ margin: '0 5px' }}>{todo.name}</strong>
-              <small style={{ margin: '0 5px' }}>Importance: {todo.importance ? 'Yes' : 'No'}</small>
-              <small style={{ margin: '0 5px' }}>Urgency: {todo.urgency ? 'Yes' : 'No'}</small>
-              <small style={{ margin: '0 5px' }}>Completed At: {new Date(todo.updated_at).toLocaleString()}</small>
-            </div>
-            <button onClick={() => handleRemoveTodo(todo.id)} style={{ marginLeft: '10px' }} className='red-button'>Remove</button>
-          </div>
-        ))}
-        <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '20px' }}>
-          <button onClick={handlePrevCompletedPage} disabled={completedPage === 1} className='green-button'>Previous</button>
-          <p>Page {completedPage} of {totalCompletedPages}</p>
-          <button onClick={handleNextCompletedPage} disabled={completedPage === totalCompletedPages} className='green-button'>Next</button>
-        </div>
+
+      {/* Tabs for switching between Completed and Ended Todos */}
+      <div style={{ marginBottom: '20px', display: 'flex', justifyContent: 'center' }}>
+        <button onClick={() => setSelectedTab('completed')} className={`tab-button ${selectedTab === 'completed' ? 'active' : ''}`}>
+          Completed Todos
+        </button>
+        <button onClick={() => setSelectedTab('ended')} className={`tab-button ${selectedTab === 'ended' ? 'active' : ''}`}>
+          Ended Todos
+        </button>
       </div>
+      {/* Conditional rendering for Completed and Ended Todos */}
+
+      {selectedTab === 'completed' && (
+        <>
+          <div style={{ marginTop: '20px', width: '80%' }}>
+            <h3>Completed Todos</h3>
+            {completedTodos.map(todo => (
+              <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                <div style={{
+                  marginBottom: '10px',
+                  padding: '10px',
+                  borderRadius: '5px',
+                  backgroundColor:
+                    todo.importance && todo.urgency ? 'red' :
+                      todo.importance && !todo.urgency ? 'orange' :
+                        !todo.importance && todo.urgency ? '#FFD700' : 'green',
+                  color:
+                    todo.importance && todo.urgency ? 'white' :
+                      todo.importance && !todo.urgency ? 'white' :
+                        !todo.importance && todo.urgency ? 'black' : 'white'
+                }}>
+                  <strong style={{ margin: '0 5px' }}>{todo.name}</strong>
+                  <small style={{ margin: '0 5px' }}>Importance: {todo.importance ? 'Yes' : 'No'}</small>
+                  <small style={{ margin: '0 5px' }}>Urgency: {todo.urgency ? 'Yes' : 'No'}</small>
+                  <small style={{ margin: '0 5px' }}>Completed At: {new Date(todo.updated_at).toLocaleString()}</small>
+                </div>
+                <button onClick={() => handleRemoveTodo(todo.id)} style={{ marginLeft: '10px' }} className='red-button'>Remove</button>
+              </div>
+            ))}
+            <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '20px' }}>
+              <button onClick={handlePrevCompletedPage} disabled={completedPage === 1} className='green-button'>Previous</button>
+              <p>Page {completedPage} of {totalCompletedPages}</p>
+              <button onClick={handleNextCompletedPage} disabled={completedPage === totalCompletedPages} className='green-button'>Next</button>
+            </div>
+          </div>
+        </>
+      )}
+
+      {selectedTab === 'ended' && (
+        <>
+          <div style={{ marginTop: '20px', width: '80%' }}>
+            <h3>Ended Todos</h3>
+            {endedTodos.map(todo => (
+              <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                <div style={{
+                  marginBottom: '10px',
+                  padding: '10px',
+                  borderRadius: '5px',
+                  backgroundColor:
+                    todo.importance && todo.urgency ? 'gray' :
+                      todo.importance && !todo.urgency ? 'darkorange' :
+                        !todo.importance && todo.urgency ? '#FFEB3B' : 'lightgray',
+                  color:
+                    todo.importance && todo.urgency ? 'white' :
+                      todo.importance && !todo.urgency ? 'black' :
+                        !todo.importance && todo.urgency ? 'black' : 'black'
+                }}>
+                  <strong style={{ margin: '0 5px' }}>{todo.name}</strong>
+                  <small style={{ margin: '0 5px' }}>Importance: {todo.importance ? 'Yes' : 'No'}</small>
+                  <small style={{ margin: '0 5px' }}>Urgency: {todo.urgency ? 'Yes' : 'No'}</small>
+                  <small style={{ margin: '0 5px' }}>Ended At: {new Date(todo.updated_at).toLocaleString()}</small>
+                </div>
+              </div>
+            ))}
+
+            <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '20px' }}>
+              <button onClick={handlePrevEndedPage} disabled={endedPage === 1} className='green-button'>Previous</button>
+              <p>Page {completedPage} of {totalCompletedPages}</p>
+              <button onClick={handleNextEndedPage} disabled={endedPage === totalEndedPages} className='green-button'>Next</button>
+            </div>
+
+          </div>
+        </>
+      )}
+
+
 
 
 
