ef85c43a9551fdfb0b1f999726b93d9898fec728
tuxiantian <tuxiantian@163.com>
2025-05-24 21:37:17 +0800

    简化页面移除已完成的待办事项查看

--- src/components/todolist/TodoList.js
+++ src/components/todolist/TodoList.js
@@ -6,14 +6,6 @@ import TodoQuadrant from './TodoQuadrant';
 
 const TodoList = ({ }) => {
   const [todos, setTodos] = useState([]);
-  const [completedTodos, setCompletedTodos] = useState([]);
-  const [endedTodos, setEndedTodos] = useState([]);
-  const [selectedTab, setSelectedTab] = useState('completed'); // 新增状态，用于切换选项卡
-  const [completedPage, setCompletedPage] = useState(1);
-  const [endedPage, setEndedPage] = useState(1);
-  const [totalCompletedPages, setTotalCompletedPages] = useState(1);
-  const [totalEndedPages, setTotalEndedPages] = useState(1);
-  const pageSize = 10;
   const [newTodoName, setNewTodoName] = useState('');
   const [newTodoDueDate, setNewTodoDueDate] = useState('');
   const [newTodoType, setNewTodoType] = useState('today');
@@ -28,8 +20,6 @@ const TodoList = ({ }) => {
 
   useEffect(() => {
     fetchTodos();
-    fetchCompletedTodos(completedPage);
-    fetchEndedTodos(endedPage);
     // 每 20 分钟刷新一次数据
     const interval = setInterval(() => {
       fetchTodos();
@@ -39,14 +29,6 @@ const TodoList = ({ }) => {
     return () => clearInterval(interval);
   }, []);
 
-  // 当 completedPage 发生变化时重新获取已完成的待办事项
-  useEffect(() => {
-    fetchCompletedTodos(completedPage);
-  }, [completedPage]);
-
-  useEffect(() => {
-    fetchEndedTodos(endedPage);
-  }, [endedPage]);
 
   const fetchTodos = () => {
     api.get(`${API_BASE_URL}/todos`)
@@ -58,63 +40,6 @@ const TodoList = ({ }) => {
       });
   };
 
-  const fetchCompletedTodos = (page) => {
-    api.get(`${API_BASE_URL}/todos/completed`, {
-      params: {
-        page,
-        page_size: pageSize
-      }
-    })
-      .then(response => {
-        setCompletedTodos(response.data.todos);
-        setTotalCompletedPages(response.data.total_pages);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the completed todos!', error);
-      });
-
-  };
-
-  // 新增方法，获取已结束的 Todo
-  const fetchEndedTodos = (page) => {
-    api.get(`${API_BASE_URL}/todos/ended`, {
-      params: {
-        page,
-        page_size: pageSize
-      }
-    })
-      .then(response => {
-        setEndedTodos(response.data.todos);
-        setTotalEndedPages(response.data.total_pages);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the ended todos!', error);
-      });
-  };
-
-  const handleNextCompletedPage = () => {
-    if (completedPage < totalCompletedPages) {
-      setCompletedPage(completedPage + 1);
-    }
-  };
-
-  const handlePrevCompletedPage = () => {
-    if (completedPage > 1) {
-      setCompletedPage(completedPage - 1);
-    }
-  };
-
-  const handleNextEndedPage = () => {
-    if (endedPage < totalEndedPages) {
-      setEndedPage(endedPage + 1);
-    }
-  };
-
-  const handlePrevEndedPage = () => {
-    if (endedPage > 1) {
-      setEndedPage(endedPage - 1);
-    }
-  };
 
   const handleAddTodo = () => {
     if (newTodoName) {
@@ -212,7 +137,6 @@ const TodoList = ({ }) => {
       .then(() => {
         // 更新状态后的操作，刷新待办列表和已完成列表
         fetchTodos(); // 更新待办事项列表
-        fetchCompletedTodos(completedPage); // 更新已完成事项列表
       })
       .catch(error => {
         console.error('There was an error updating the todo!', error);
@@ -223,7 +147,6 @@ const TodoList = ({ }) => {
     api.delete(`${API_BASE_URL}/todos/${id}`)
       .then(() => {
         fetchTodos(); // 更新待办事项列表
-        fetchCompletedTodos(completedPage); // 更新已完成事项列表
       })
       .catch(error => {
         console.error('There was an error deleting the todo!', error);
@@ -311,94 +234,6 @@ const TodoList = ({ }) => {
 
         </div>
       </div>
-
-      {/* Tabs for switching between Completed and Ended Todos */}
-      <div style={{ margin: '20px auto', display: 'flex', justifyContent: 'center', width: '600px' }}>
-        <button onClick={() => setSelectedTab('completed')} className={`tab-button ${selectedTab === 'completed' ? 'active' : ''}`}>
-          Completed Todos
-        </button>
-        <button onClick={() => setSelectedTab('ended')} className={`tab-button ${selectedTab === 'ended' ? 'active' : ''}`}>
-          Ended Todos
-        </button>
-      </div>
-      {/* Conditional rendering for Completed and Ended Todos */}
-
-      {selectedTab === 'completed' && (
-        <>
-          <div style={{ marginTop: '20px', width: '80%' }}>
-            <h3>Completed Todos</h3>
-            {completedTodos.map(todo => (
-              <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                <div style={{
-                  marginBottom: '10px',
-                  padding: '10px',
-                  borderRadius: '5px',
-                  backgroundColor:
-                    todo.importance && todo.urgency ? 'red' :
-                      todo.importance && !todo.urgency ? 'orange' :
-                        !todo.importance && todo.urgency ? '#FFD700' : 'green',
-                  color:
-                    todo.importance && todo.urgency ? 'white' :
-                      todo.importance && !todo.urgency ? 'white' :
-                        !todo.importance && todo.urgency ? 'black' : 'white'
-                }}>
-                  <strong style={{ margin: '0 5px' }}>{todo.name}</strong>
-                  <small style={{ margin: '0 5px' }}>Importance: {todo.importance ? 'Yes' : 'No'}</small>
-                  <small style={{ margin: '0 5px' }}>Urgency: {todo.urgency ? 'Yes' : 'No'}</small>
-                  <small style={{ margin: '0 5px' }}>Completed At: {new Date(todo.updated_at).toLocaleString()}</small>
-                </div>
-                <button onClick={() => handleRemoveTodo(todo.id)} style={{ marginLeft: '10px' }} className='red-button'>Remove</button>
-              </div>
-            ))}
-            <div style={{ display: 'flex', justifyContent: 'flex-start', margin: '20px auto' }}>
-              <button onClick={handlePrevCompletedPage} disabled={completedPage === 1} className='green-button'>Previous</button>
-              <p style={{ margin: '0 10px', display: 'flex', alignItems: 'center' }}>Page {completedPage} of {totalCompletedPages}</p>
-              <button onClick={handleNextCompletedPage} disabled={completedPage === totalCompletedPages} className='green-button'>Next</button>
-            </div>
-          </div>
-        </>
-      )}
-
-      {selectedTab === 'ended' && (
-        <>
-          <div style={{ marginTop: '20px', width: '80%' }}>
-            <h3>Ended Todos</h3>
-            {endedTodos.map(todo => (
-              <div key={todo.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                <div style={{
-                  marginBottom: '10px',
-                  padding: '10px',
-                  borderRadius: '5px',
-                  backgroundColor:
-                    todo.importance && todo.urgency ? 'gray' :
-                      todo.importance && !todo.urgency ? 'darkorange' :
-                        !todo.importance && todo.urgency ? '#FFEB3B' : 'lightgray',
-                  color:
-                    todo.importance && todo.urgency ? 'white' :
-                      todo.importance && !todo.urgency ? 'black' :
-                        !todo.importance && todo.urgency ? 'black' : 'black'
-                }}>
-                  <strong style={{ margin: '0 5px' }}>{todo.name}</strong>
-                  <small style={{ margin: '0 5px' }}>Importance: {todo.importance ? 'Yes' : 'No'}</small>
-                  <small style={{ margin: '0 5px' }}>Urgency: {todo.urgency ? 'Yes' : 'No'}</small>
-                  <small style={{ margin: '0 5px' }}>Ended At: {new Date(todo.updated_at).toLocaleString()}</small>
-                </div>
-              </div>
-            ))}
-
-            <div style={{ display: 'flex', justifyContent: 'flex-start', margin: '20px auto' }}>
-              <button onClick={handlePrevEndedPage} disabled={endedPage === 1} className='green-button'>Previous</button>
-              <p style={{ margin: '0 10px', display: 'flex', alignItems: 'center' }}>Page {completedPage} of {totalCompletedPages}</p>
-              <button onClick={handleNextEndedPage} disabled={endedPage === totalEndedPages} className='green-button'>Next</button>
-            </div>
-
-          </div>
-        </>
-      )}
-
-
-
-
 
     </div>
   );
