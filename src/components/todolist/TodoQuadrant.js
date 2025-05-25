d2ad1352733f66a491e7fa4e603b7e14b1db354e
tuxiantian <tuxiantian@163.com>
2025-05-25 21:50:29 +0800

    优化todolist的时间显示

--- src/components/todolist/TodoQuadrant.js
+++ src/components/todolist/TodoQuadrant.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tooltip } from 'react-tooltip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -22,10 +23,37 @@ const TodoQuadrant = ({ todos, onTodoCompletion, onRemoveTodo }) => {
                             style={{ marginRight: '10px' }}
                         />
                         <div style={{ display: 'flex', alignItems: 'center', flex: 1 }}>
-                            <strong style={{ marginRight: '10px' }}>{todo.name}</strong>
+                            <strong style={{ marginRight: '10px',textAlign:'left' }}>{todo.name}</strong>
                             <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                <small style={{ marginBottom: '5px' }}>Start: {startTime.toLocaleString()}</small>
-                                <small>End: {endTime.toLocaleString()}</small>
+
+                                {/* 开始时间图标+Tooltip */}
+                                <div title={`开始时间: ${startTime.toLocaleString()}`} style={{ position: 'relative' }}
+                                    data-tip={`开始时间: ${startTime.toLocaleString()}`}
+                                    data-for="start-time-tooltip">
+                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="12" cy="12" r="9" stroke="#666666" strokeWidth="2" />
+                                        <path d="M12 6V12" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                        <path d="M12 12L16 14" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                        <circle cx="12" cy="12" r="2" fill="#666666" />
+                                        <path d="M7 3L7 6M17 3L17 6" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                    </svg>
+                                </div>
+                                <Tooltip id="start-time-tooltip" effect="solid" />
+
+                                {/* 结束时间图标+Tooltip */}
+                                <div title={`结束时间: ${endTime.toLocaleString()}`} style={{ position: 'relative' }}
+                                    data-tip={`结束时间: ${endTime.toLocaleString()}`}
+                                    data-for="end-time-tooltip">
+                                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="12" cy="12" r="9" stroke="#666666" strokeWidth="2" />
+                                        <path d="M12 6V12" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                        <path d="M12 12L8 15" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                        <circle cx="12" cy="12" r="2" fill="#666666" />
+                                        <path d="M7 3L7 6M17 3L17 6" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                        <path d="M6 19L18 19" stroke="#666666" strokeWidth="2" strokeLinecap="round" />
+                                    </svg>
+                                </div>
+                                <Tooltip id="end-time-tooltip" effect="solid" />
                             </div>
                         </div>
                         <div
