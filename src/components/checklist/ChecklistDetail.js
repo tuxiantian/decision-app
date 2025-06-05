b263768a2fc8e3bba6d5e3e5a63cf19271a49b02
tuxiantian <tuxiantian@163.com>
2025-06-05 08:28:55 +0800

    移除没有用的代码

--- src/components/checklist/ChecklistDetail.js
+++ src/components/checklist/ChecklistDetail.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
 import PersonalStateCheck from './PersonalStateCheck.js';
