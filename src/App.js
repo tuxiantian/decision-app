3b93a47ce6bf24ebeede776724493a26e3948ce7
tuxiantian <tuxiantian@163.com>
2025-06-19 21:24:22 +0800

    增加LangingPage页面

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ import InspirationClub from './components/inspiration/InspirationClub';
 import MyReflections from './components/inspiration/MyReflections';
 import FlowTest from './components/checklist/FlowTest';
 import MermaidTool from './components/checklist/MermaidTool';
+import LandingPage from './LandingPage';
 
 function App() {
   const [username, setUsername] = useState(null); // 存储用户名
@@ -73,182 +74,190 @@ function App() {
     }
   };
 
-    // 定义菜单结构
-    const menuItems = [
-      {
-        title: '首页',
-        path: '/'
-      },{
-        title: '流程图',
-        path: '/flow'
-      },
-      {
-        title: '平衡决策',
-        children: [
-          {
-            title: '去做平衡决策',
-            path: '/balanced-decisions'
-          },
-          {
-            title: '平衡决策列表',
-            path: '/balanced-decisions/list'
-          }
-        ]
-      },
-      {
-        title: '决策清单',
-        children: [
-          {
-            title: '我的决策清单列表',
-            path: '/checklists'
-          },
-          {
-            title: '新建决策清单',
-            path: '/checklist-form'
-          },
-          {
-            title: '我做的决定',
-            path: '/history'
-          }
-        ]
-      },
-      {
-        title: '文章',
-        children: [
-          {
-            title: '文章列表',
-            path: '/articles'
-          },
-          {
-            title: '添加文章',
-            path: '/add-article'
-          }
-        ]
-      },
-      {
-        title: 'AHP分析',
-        children: [
-          {
-            title: '我做过的AHP分析',
-            path: '/ahp'
-          },
-          {
-            title: '去做AHP分析',
-            path: '/ahp-add'
-          }
-        ]
-      },
-      {
-        title: '待办',
-        children: [
-          {
-            title: '待办事项',
-            path: '/todos'
-          },
-          {
-            title: '完成的待办事项',
-            path: '/history-todos'
-          }
-        ]
-      },
-      
-      {
-        title: '启发',
-        children: [
-          {
-            title: '启发俱乐部',
-            path: '/inspiration'
-          },
-          {
-            title: '我的感想',
-            path: '/reflections'
-          }
-        ]
-      },
-      {
-        title: '更多',
-        children: [
-          {
-            title: '事实-观点',
-            children: [
-              {
-                title: '事实观点分析',
-                path: '/argument-evaluator'
-              },
-              {
-                title: '事实观点分析列表',
-                path: '/argument-evaluator-list'
-              }
-            ]
-          },{
-            title: '凯利计算器',
-            path: '/kelly'
-          },{
-            title: 'mermaid画图',
-            path: '/mermaid'
-          },
-          {
-            title: '反馈',
-            path: '/feedback'
-          },
-          {
-            title: '我的反馈',
-            path: '/myfeedback'
-          },
-          {
-            title: '关于我们',
-            path: '/about-us'
-          }
-        ]
-      }
-    ];
+  // 定义菜单结构
+  const menuItems = [
+    {
+      title: '首页',
+      path: '/home'
+    }, {
+      title: '流程图',
+      path: '/flow'
+    },
+    {
+      title: '平衡决策',
+      children: [
+        {
+          title: '去做平衡决策',
+          path: '/balanced-decisions'
+        },
+        {
+          title: '平衡决策列表',
+          path: '/balanced-decisions/list'
+        }
+      ]
+    },
+    {
+      title: '决策清单',
+      children: [
+        {
+          title: '我的决策清单列表',
+          path: '/checklists'
+        },
+        {
+          title: '新建决策清单',
+          path: '/checklist-form'
+        },
+        {
+          title: '我做的决定',
+          path: '/history'
+        }
+      ]
+    },
+    {
+      title: '文章',
+      children: [
+        {
+          title: '文章列表',
+          path: '/articles'
+        },
+        {
+          title: '添加文章',
+          path: '/add-article'
+        }
+      ]
+    },
+    {
+      title: 'AHP分析',
+      children: [
+        {
+          title: '我做过的AHP分析',
+          path: '/ahp'
+        },
+        {
+          title: '去做AHP分析',
+          path: '/ahp-add'
+        }
+      ]
+    },
+    {
+      title: '待办',
+      children: [
+        {
+          title: '待办事项',
+          path: '/todos'
+        },
+        {
+          title: '完成的待办事项',
+          path: '/history-todos'
+        }
+      ]
+    },
+
+    {
+      title: '启发',
+      children: [
+        {
+          title: '启发俱乐部',
+          path: '/inspiration'
+        },
+        {
+          title: '我的感想',
+          path: '/reflections'
+        }
+      ]
+    },
+    {
+      title: '更多',
+      children: [
+        {
+          title: '事实-观点',
+          children: [
+            {
+              title: '事实观点分析',
+              path: '/argument-evaluator'
+            },
+            {
+              title: '事实观点分析列表',
+              path: '/argument-evaluator-list'
+            }
+          ]
+        }, {
+          title: '凯利计算器',
+          path: '/kelly'
+        }, {
+          title: 'mermaid画图',
+          path: '/mermaid'
+        },
+        {
+          title: '反馈',
+          path: '/feedback'
+        },
+        {
+          title: '我的反馈',
+          path: '/myfeedback'
+        },
+        {
+          title: '关于我们',
+          path: '/about-us'
+        }
+      ]
+    }
+  ];
 
   return (
 
     <div className="App">
-      <CustomMenu 
-        menuItems={menuItems} 
-        username={username} 
-        onLogout={handleLogout} 
-      />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/*" element={
+          <>
+            <CustomMenu
+              menuItems={menuItems}
+              username={username}
+              onLogout={handleLogout}
+            />
+            <Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/flow" element={<FlowTest />} />
-        <Route path="/login" element={<LoginPage onLogin={setUsername} />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/join-group/:groupId" element={<JoinGroupPage />} />
-        <Route path="/questionnaire/:decisionId" element={<Questionnaire />} />
-        <Route path="/balanced-decisions" element={<BalancedDecisionMaker />} />
-        <Route path="/balanced-decisions/list" element={<BalancedDecisionList />} />
-        <Route path="/balanced-decisions/:id" element={<BalancedDecisionDetail />} />
-        <Route path="/ahp" element={<AHPAnalysis />} />
-        <Route path="/ahp-add" element={<PairwiseComparison />} />
-        <Route path="/checklist-form" element={<ChecklistForm />} />
-        <Route path="/checklists" element={<ChecklistList />} />
-        <Route path="/checklist/:checklistId" element={<ChecklistDetail />} />
-        <Route path="/history" element={<ChecklistAnswerHistory />} />
-        <Route path="/checklist_answers/details/:decisionId" element={<DecisionDetails />} />
-        <Route path="/checklist/update/:checklistId" element={<ChecklistForm />} />
-        <Route path="/todos" element={<TodoList />} />
-        <Route path="/history-todos" element={<TodoHistory />} />
-        <Route path='/kelly' element={<KellyCalculator />} />
-        <Route path='/mermaid' element={<MermaidTool />}/>
-        <Route path='/inspiration' element={<InspirationClub />} />
-        <Route path='/reflections' element={<MyReflections />} />
-        <Route path="/articles" element={<ArticleList />} />
-        <Route path="/add-article" element={<ArticleEditor />} />
-        <Route path="/edit-article/:id" element={<ArticleEditor />} />
-        <Route path="/view-article/:source/:id" element={<ArticleViewer />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/checklist/:decisionId/review" element={<ReviewEditor />} />
-        <Route path="/checklist/flowchart/:checklistId" element={<FlowchartDetail />} />
-        <Route path="/argument-evaluator" element={<FactOpinionAnalyzer />} />
-        <Route path="/argument-evaluator-list" element={<AnalysisList />} />
-        <Route path="/analysis-detail/:id" element={<AnalysisDetail />} />
-        <Route path="/feedback" element={<FeedbackForm />} />
-        <Route path="/myfeedback" element={<MyFeedback />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/flow" element={<FlowTest />} />
+              <Route path="/login" element={<LoginPage onLogin={setUsername} />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/join-group/:groupId" element={<JoinGroupPage />} />
+              <Route path="/questionnaire/:decisionId" element={<Questionnaire />} />
+              <Route path="/balanced-decisions" element={<BalancedDecisionMaker />} />
+              <Route path="/balanced-decisions/list" element={<BalancedDecisionList />} />
+              <Route path="/balanced-decisions/:id" element={<BalancedDecisionDetail />} />
+              <Route path="/ahp" element={<AHPAnalysis />} />
+              <Route path="/ahp-add" element={<PairwiseComparison />} />
+              <Route path="/checklist-form" element={<ChecklistForm />} />
+              <Route path="/checklists" element={<ChecklistList />} />
+              <Route path="/checklist/:checklistId" element={<ChecklistDetail />} />
+              <Route path="/history" element={<ChecklistAnswerHistory />} />
+              <Route path="/checklist_answers/details/:decisionId" element={<DecisionDetails />} />
+              <Route path="/checklist/update/:checklistId" element={<ChecklistForm />} />
+              <Route path="/todos" element={<TodoList />} />
+              <Route path="/history-todos" element={<TodoHistory />} />
+              <Route path='/kelly' element={<KellyCalculator />} />
+              <Route path='/mermaid' element={<MermaidTool />} />
+              <Route path='/inspiration' element={<InspirationClub />} />
+              <Route path='/reflections' element={<MyReflections />} />
+              <Route path="/articles" element={<ArticleList />} />
+              <Route path="/add-article" element={<ArticleEditor />} />
+              <Route path="/edit-article/:id" element={<ArticleEditor />} />
+              <Route path="/view-article/:source/:id" element={<ArticleViewer />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route path="/checklist/:decisionId/review" element={<ReviewEditor />} />
+              <Route path="/checklist/flowchart/:checklistId" element={<FlowchartDetail />} />
+              <Route path="/argument-evaluator" element={<FactOpinionAnalyzer />} />
+              <Route path="/argument-evaluator-list" element={<AnalysisList />} />
+              <Route path="/analysis-detail/:id" element={<AnalysisDetail />} />
+              <Route path="/feedback" element={<FeedbackForm />} />
+              <Route path="/myfeedback" element={<MyFeedback />} />
+            </Routes>
+          </>
+        } />
       </Routes>
+
     </div>
 
   );
